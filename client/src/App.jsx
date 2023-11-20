73332fe2ec88ecdea75496c50fb9cdaafb8f74cf
MrWhiffing <anishmanna2003@gmail.com>
2023-11-21 00:10:45 +0530

    first template using react and tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,7 @@
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import About from "./pages/About";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-
-
+import React from 'react'
 
 export default function App() {
   return (
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/profile' element={<Profile />} />
-      <Route path='/sign-up' element={<SignUp />} />
-      <Route path='/sign-in' element={<SignIn />} />
-    </Routes>
-  </BrowserRouter>);
+    <div>App</div>
+  )
 }
