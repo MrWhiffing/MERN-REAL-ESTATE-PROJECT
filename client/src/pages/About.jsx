4cd271dea1a5ceb4c12f37d19a32cd6125fc9656
MrWhiffing <anishmanna2003@gmail.com>
2023-11-24 22:00:50 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-8 mt-6 text-slate-800'>About SereneSeek</h1>
+      <p className='mb-4 text-slate-700'>SereneSeek is a premier real estate platform dedicated to simplifying the property search experience for buyers and sellers. Our platform seamlessly connects individuals with their dream homes while fostering effortless communication and transactions.</p>
+      <p className='mb-4 text-slate-700'>
+      At SereneSeek, our mission is to redefine real estate by offering a user-friendly platform that streamlines the buying and selling process. Our experienced team is committed to delivering unparalleled service, expert guidance, and a profound understanding of the local real estate landscape.
+      </p>
+      <p className='mb-4 text-slate-700'>With a focus on creating a serene and rewarding experience, SereneSeek empowers clients to achieve their real estate goals. Whether you're in search of your dream home or looking to sell a property, our dedicated team is here to support you at every step.</p>
+      <p className='mb-4 text-slate-700'>Choose SereneSeek for a seamless journey in the world of real estate. We believe that finding or selling a property should be an enjoyable venture, and we are committed to making that vision a reality for each of our valued clients.</p>
+    </div>
+  );
 }
